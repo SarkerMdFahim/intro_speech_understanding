06468ddf557d03a404acdea4493a8df9694141d0
SarkerMdFahim_M25W0499 <mafahim100sarker@gmail.com>
2026-04-26 08:27:22 +0900

    Update homework2.py

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -5,12 +5,22 @@ that method, type `help homework2.arithmetic`.
 
 def arithmetic(x, y):
     """
-    Modify this code so that it performs one of four possible functions, 
+    Modify this code so that it performs one of four possible functions,
     as specified in the following table:
 
                         isinstance(x,str)  isinstance(x,float)
     isinstance(y,str)   return x+y         return str(x)+y
     isinstance(y,float) return x*int(y)    return x*y
     """
-    return 0
 
+    if isinstance(y, str):
+        if isinstance(x, str):
+            return x + y
+        elif isinstance(x, float):
+            return str(x) + y
+
+    elif isinstance(y, float):
+        if isinstance(x, str):
+            return x * int(y)
+        elif isinstance(x, float):
+            return x * y
