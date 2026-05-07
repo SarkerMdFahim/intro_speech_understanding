ee9bb82b11c2a409f9e4c92462612ef8e16a5082
SarkerMdFahim_M25W0499 <mafahim100sarker@gmail.com>
2026-05-08 06:08:36 +0900

    Update homework3.py

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -1,18 +1,27 @@
-
 def words2characters(words):
     """
     This function converts a list of words into a list of characters.
-
-    @param:
-    words - a list of words
-
-    @return:
-    characters - a list of characters
-
-    Every element of "words" should be converted to a str, then split into
-    characters, each of which is separately appended to "characters." For 
-    example, if words==['hello', 1.234, True], then characters should be
-    ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
     """
-    return []
 
+    characters = []
+
+    for word in words:
+        # Convert each element to string
+        word = str(word)
+
+        # Split into characters and append
+        for char in word:
+            characters.append(char)
+
+    return characters
+
+
+# Input list
+words = ['hello', 1.234, True]
+
+# Function call
+result = words2characters(words)
+
+# Printing output
+print("Input:", words)
+print("Output:", result)
